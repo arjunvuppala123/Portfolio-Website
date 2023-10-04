cc9cb879f87f3c885fe0783ae0d269a843226e36
arjunvuppala123 <56397988+arjunvuppala123@users.noreply.github.com>
2023-10-04 11:40:17 +0530

    added the contactUs section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,30 @@ import NavBar from '../components/NavBar'
 import AboutMe from '../components/AboutMe'
 import MyEducation from '../components/MyEducation'
 import MyProjects from '../components/Projects'
+import Skills from '../components/Skills'
+import ContactUs from '../components/ContactUs'
 
 import styles from '../styles/Home.module.css'
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Arjun Vuppala | My Portfolio Website</title>
+      <meta name="description" content="Portfolio of Arjun Vuppala" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <div className={styles.container}>
       <NavBar />
       <EntrySection />
       <AboutMe />
       <MyEducation />
+      <Skills />
       <MyProjects />
+      <ContactUs />
     </div>
+    </>
   )
 }
